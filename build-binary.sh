fc1aae78cb44c45dc62d01d7d2e605bca60e8e26
Martijn van de Rijdt <martijn@enketo.org>
2018-01-04 16:40:54 -0700

    first try in Travis

--- build-binary.sh
+++ build-binary.sh
@@ -8,7 +8,8 @@ fi
 
 echo "Building binaries for ${OS}..."
 
-TARGET_MAIN="../enketo-validate-binaries/${OS}"
+TARGET_BASE="../enketo-validate-binaries"
+TARGET_OS="$TARGET_BASE/${OS}"
 NODE_VERSION="8"
 
 # assuming npm modules are currently installed for different OS
@@ -22,25 +23,26 @@ sudo npm install -g pkg
 # run build again just in case the current build is a custom build
 npm run build 
 pkg validate --targets node${NODE_VERSION}-${OS}-x64 --output validate-${OS}
-
-# custom oc build (for linux)
-if [ $OS = 'linux'] ; then
-  npm run oc-build
-  pkg validate --targets node${NODE_VERSION}-${OS}-x64 --output validate-${OS}-oc
-fi
-
-# Copy binaries
-mkdir -p ${TARGET_MAIN}
-cp ./validate-${OS} ${TARGET_MAIN}/validate-${OS}
-cp ./validate-${OS}-oc ${TARGET_MAIN}/validate-${OS}-oc
+mkdir -p ${TARGET_OS}
+mv ./validate-${OS} ${TARGET_OS}/validate-${OS}
 
 # Copy the compiled libxmljs-mt files:
-mkdir -p ${TARGET_MAIN}/node_modules/libxmljs-mt/build/Release
-cp -r node_modules/libxmljs-mt/build/Release ${TARGET_MAIN}/node_modules/libxmljs-mt/build/
+mkdir -p ${TARGET_OS}/node_modules/libxmljs-mt/build/Release
+cp -r node_modules/libxmljs-mt/build/Release ${TARGET_OS}/node_modules/libxmljs-mt/build/
 
 # Copy the compiled libxslt files:
-mkdir -p ${TARGET_MAIN}/node_modules/libxslt/build/Release
-cp -r node_modules/libxslt/build/Release ${TARGET_MAIN}/node_modules/libxslt/build/
+mkdir -p ${TARGET_OS}/node_modules/libxslt/build/Release
+cp -r node_modules/libxslt/build/Release ${TARGET_OS}/node_modules/libxslt/build/
+
+# custom oc build (for linux only)
+if [ $OS = 'linux' ] ; then
+  npm run oc-build
+  pkg validate --targets node${NODE_VERSION}-${OS}-x64 --output validate-${OS}-oc
+  mv ./validate-${OS}-oc ${TARGET_OS}/validate-${OS}-oc
+fi
+
+# Create a zip file
+tar czf ${TARGET_BASE}/${OS}.tar.gz ${TARGET_OS}/*
 
 # Test
-${TARGET_MAIN}/validate-${OS} --help
+${TARGET_OS}/validate-${OS} --help
