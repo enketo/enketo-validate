38a111ad85e14c154c22ace153212a2b276fa943
Martijn van de Rijdt <martijn@enketo.org>
2018-01-04 15:18:40 -0700

    start of linux/macos script

---
+++ build-binary.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+if [ $1 = "linux" -o $1 = "macos" ] ; then
+  OS=$1
+else
+  echo "This script needs either the 'linux' or 'macos' parameter"
+  exit 1
+fi
+
+echo "Building binaries for ${OS}..."
+
+TARGET_MAIN="../enketo-validate-binaries/${OS}"
+NODE_VERSION="8"
+
+# assuming npm modules are currently installed for different OS
+rm -R node_modules
+npm install
+
+# pkg module
+sudo npm install -g pkg
+
+# default linux
+# run build again just in case the current build is a custom build
+npm run build 
+pkg validate --targets node${NODE_VERSION}-${OS}-x64 --output validate-${OS}
+
+# oc linux
+npm run oc-build
+pkg validate --targets node${NODE_VERSION}-${OS}-x64 --output validate-${OS}-oc
+
+# copy stuffs
+mkdir -p ${TARGET_MAIN}
+cp ./validate-${OS} ${TARGET_MAIN}/validate-${OS}
+cp ./validate-${OS}-oc ${TARGET_MAIN}/validate-${OS}-oc
+
+# Copy the compiled libxmljs-mt files:
+mkdir -p ${TARGET_MAIN}/node_modules/libxmljs-mt/build/Release
+cp -r node_modules/libxmljs-mt/build/Release ${TARGET_MAIN}/node_modules/libxmljs-mt/build/
+
+# Copy the compiled libxslt files:
+mkdir -p ${TARGET_MAIN}/node_modules/libxslt/build/Release
+cp -r node_modules/libxslt/build/Release ${TARGET_MAIN}/node_modules/libxslt/build/
+
+# Test
+${TARGET_MAIN}/validate-linux --me
