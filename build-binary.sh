392bd6139966fb459100d62496abf1e183cbb133
Martijn van de Rijdt <martijn@enketo.org>
2018-01-08 16:50:48 -0700

    again

--- build-binary.sh
+++ build-binary.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "TRAVIS_OS_NAME:${TRAVIS_OS_NAME}"
+echo "TRAVIS_OS_NAME: ${TRAVIS_OS_NAME}"
 
 if [ "$TRAVIS_OS_NAME" = "osx" ] ; then
   OS="macos"
@@ -17,16 +17,7 @@ TARGET_OS="$TARGET_BASE/${OS}"
 FILENAME="validate"
 NODE_VERSION="8"
 
-# assuming npm modules are currently installed for different OS
-# rm -R node_modules
-# npm install
-
-# Install pkg module if it doesn't already exist
-# sudo npm install -g pkg
-
-# default linux
-# run build again just in case the current build is a custom build
-# npm run build 
+# Create the binary 
 pkg validate --targets node${NODE_VERSION}-${OS}-x64 --output validate-${OS}
 mkdir -p ${TARGET_OS}
 mv ./validate-${OS} ${TARGET_OS}/${FILENAME}
@@ -39,20 +30,22 @@ cp -r node_modules/libxmljs-mt/build/Release ${TARGET_OS}/node_modules/libxmljs-
 mkdir -p ${TARGET_OS}/node_modules/libxslt/build/Release
 cp -r node_modules/libxslt/build/Release ${TARGET_OS}/node_modules/libxslt/build/
 
-# Create a zip file
-#tar czf ${OS}.tar.gz ${TARGET_OS}
-zip -r9 ${OS}.zip ${TARGET_OS}
-
-# custom oc build (for linux only)
+# Custom oc build (for linux only)
 if [ "$OS" = "linux" ] ; then
   npm run oc-build
   mkdir -p ${TARGET_OS}-oc
   pkg validate --targets node${NODE_VERSION}-${OS}-x64 --output validate-${OS}-oc
-  mv ./validate-${OS}-oc ${TARGET_OS}/${FILENAME}
+  mv ./validate-${OS}-oc ${TARGET_OS}-oc/${FILENAME}
   cp -r ${TARGET_OS}/node_modules/libxmljs-mt/build/ ${TARGET_OS}-oc/node_modules/libxmljs-mt/build/
   cp -r ${TARGET_OS}/node_modules/libxslt/build/ ${TARGET_OS}-oc/node_modules/libxslt/build/
-  zip -r9 ${OS}-oc.zip ${TARGET_OS}
+fi
+
+# Create a zip file
+cd ${TARGET_BASE}
+zip -r9 ${OS}.zip ${OS}
+if [ "$OS" = "linux" ] ; then
+  zip -r9 ${OS}-oc.zip ${OS}-oc
 fi
 
 # Test (a valid form, so it exits with 0)
-${TARGET_OS}/${FILENAME} test/xform/model-only.xml
+./${OS}/${FILENAME} ../enketo-validate/test/xform/model-only.xml
