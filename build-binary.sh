882644f09dac4baad3e6d7882355fab727118960
Martijn van de Rijdt <martijn@enketo.org>
2018-01-08 14:42:14 -0700

    again

--- build-binary.sh
+++ build-binary.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-if [ $1 = "linux" -o $1 = "osx" ] ; then
-  OS=$1
+if [ $TRAVIS_OS_NAME = "osx" ] ; then
+  OS = "macos"
+elif [ $TRAVIS_OS_NAME = "linux" -o  $TRAVIS_OS_NAME = "macos" ] ; then
+  OS=$TRAVIS_OS_NAME
 else
-  echo "This script needs to be run with either the 'linux' or 'macos' parameter"
+  echo "This script needs to be run with \$TRAVIS_OS_NAME value 'linux', 'macos' or 'osx'"
   exit 1
 fi
 
@@ -10,6 +12,7 @@ echo "Building binaries for ${OS}..."
 
 TARGET_BASE="../enketo-validate-binaries"
 TARGET_OS="$TARGET_BASE/${OS}"
+FILENAME="validate"
 NODE_VERSION="8"
 
 # assuming npm modules are currently installed for different OS
@@ -24,7 +27,7 @@ NODE_VERSION="8"
 # npm run build 
 pkg validate --targets node${NODE_VERSION}-${OS}-x64 --output validate-${OS}
 mkdir -p ${TARGET_OS}
-mv ./validate-${OS} ${TARGET_OS}/validate-${OS}
+mv ./validate-${OS} ${TARGET_OS}/${FILENAME}
 
 # Copy the compiled libxmljs-mt files:
 mkdir -p ${TARGET_OS}/node_modules/libxmljs-mt/build/Release
@@ -34,15 +37,20 @@ cp -r node_modules/libxmljs-mt/build/Release ${TARGET_OS}/node_modules/libxmljs-
 mkdir -p ${TARGET_OS}/node_modules/libxslt/build/Release
 cp -r node_modules/libxslt/build/Release ${TARGET_OS}/node_modules/libxslt/build/
 
+# Create a zip file
+#tar czf ${OS}.tar.gz ${TARGET_OS}
+zip -r9 ${OS}.zip ${TARGET_OS}
+
 # custom oc build (for linux only)
 if [ $OS = 'linux' ] ; then
   npm run oc-build
+  mkdir -p ${TARGET_OS}-oc
   pkg validate --targets node${NODE_VERSION}-${OS}-x64 --output validate-${OS}-oc
-  mv ./validate-${OS}-oc ${TARGET_OS}/validate-${OS}-oc
+  mv ./validate-${OS}-oc ${TARGET_OS}/${FILENAME}
+  cp -r ${TARGET_OS}/node_modules/libxmljs-mt/build/ ${TARGET_OS}-oc/node_modules/libxmljs-mt/build/
+  cp -r ${TARGET_OS}/node_modules/libxslt/build/ ${TARGET_OS}-oc/node_modules/libxslt/build/
+  zip -r9 ${OS}-oc.zip ${TARGET_OS}
 fi
 
-# Create a zip file
-tar czf ${OS}.tar.gz ${TARGET_OS}
-
 # Test
-${TARGET_OS}/validate-${OS} --help
+${TARGET_OS}/${FILENAME} --help
