6583d6b8f2e1cbe1b2214819e302c9a11f6ecbf7
Martijn van de Rijdt <martijn@enketo.org>
2018-01-10 17:06:34 -0700

    tweaks

--- windows-build-binary.bat
+++ windows-build-binary.bat
@@ -1,16 +1,17 @@
 SET OS=windows
-
-ECHO "Building binaries for ${OS}..."
-
 SET TARGET_BASE="enketo-validate-binaries"
 SET TARGET_OS="%TARGET_BASE%\%OS%"
 SET FILENAME="validate"
 SET NODE_VERSION="8"
 
+ECHO Building binaries for %OS%...
+
 REM Create the binary 
 pkg validate --targets node%NODE_VERSION%-win-x64 --output validate-%OS%
 MKDIR %TARGET_OS%
 MOVE validate-%OS%.exe %TARGET_OS%\%FILENAME%
+
+ECHO Copying the node_module packages
 
 REM Copy the compiled libxmljs-mt files:
 MKDIR %TARGET_OS%\node_modules\libxmljs-mt\build\Release
@@ -20,6 +21,8 @@ REM Copy the compiled libxslt files:
 MKDIR %TARGET_OS%\node_modules\libxslt\build\Release
 COPY node_modules\libxslt\build\Release\*.node %TARGET_OS%\node_modules\libxslt\build\Release\
 
-ECHO "All done, let us check if it works:"
+ECHO Done copying
+
+ECHO All done, let us check if it works:
 REM Test (a valid form, so it exits with 0) 
 %OS%\%FILENAME% test\xform\model-only.xml
