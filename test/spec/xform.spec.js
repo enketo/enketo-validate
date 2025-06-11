4ab5ae76e990fb8299e95e475dfc63ca6d6056c2
Kalvis Kalniņš <kalvis.kalnins@kobotoolbox.org>
2025-06-11 19:20:53 +0300

    wip: increase timeout

--- test/spec/xform.spec.js
+++ test/spec/xform.spec.js
@@ -12,10 +12,9 @@ describe( 'XForm', () => {
 
     describe( 'that is valid', () => {
         const xf = loadXForm( 'model-only.xml' );
-        it( 'returns duration', async(done) => {
+        it( 'returns duration', async() => {
             const result = await validator.validate( xf );
-            expect(result.duration).to.be.above(0);
-            done()
+            expect( result.duration ).to.be.above( 0 );
         } );
 
         it( 'returns no errors and no warnings', async() => {
